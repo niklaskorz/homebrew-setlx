6be0d480c9efa8c666114fbb2747ecce1d3c9018
Niklas Korz <korz.niklask@gmail.com>
2017-04-09 13:18:13 +0200

    Update to SetlX 2.6.1

--- setlx.rb
+++ setlx.rb
@@ -1,9 +1,9 @@
 class Setlx < Formula
   desc "SetlX is an interpreter for the high level programming-language SetlX (set language extended)."
   homepage "https://randoom.org/Software/SetlX"
-  url "http://download.randoom.org/setlX/pc/setlX_v2-6-0.binary_only.zip"
-  version "2.6.0"
-  sha256 "e3627decb0c0fd1fb00f202d4647c2026d28e61e7d9e0d3edbd4dd8a86d9f025"
+  url "http://download.randoom.org/setlX/pc/setlX_v2-6-1.binary_only.zip"
+  version "2.6.1"
+  sha256 "a88b3dca6e03667adfd0d62e628c241fa6e0c5b28ef95530da9febe5b4377cc5"
 
   depends_on :java => "1.7+"
   depends_on "rlwrap" => :recommended
