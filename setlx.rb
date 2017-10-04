8f3ac924f2da5e17965766870fd1596695d1a623
Niklas Korz <korz.niklask@gmail.com>
2017-10-04 13:16:24 +0200

    Update to SetlX 2.7.0

--- setlx.rb
+++ setlx.rb
@@ -1,9 +1,9 @@
 class Setlx < Formula
   desc "SetlX is an interpreter for the high level programming-language SetlX (set language extended)."
   homepage "https://randoom.org/Software/SetlX"
-  url "http://download.randoom.org/setlX/pc/setlX_v2-6-1.binary_only.zip"
-  version "2.6.1"
-  sha256 "a88b3dca6e03667adfd0d62e628c241fa6e0c5b28ef95530da9febe5b4377cc5"
+  url "http://download.randoom.org/setlX/pc/setlX_v2-7-0.binary_only.zip"
+  version "2.7.0"
+  sha256 "876c8aad46fc8c08e71d55fc0e6b9a46d753f16ea9655d1beae76d170b1f2de3"
 
   depends_on :java => "1.7+"
   depends_on "rlwrap" => :recommended
@@ -13,7 +13,7 @@ class Setlx < Formula
     system "sed", "-i", ".original", "s,setlXJarDirectory=\".\",setlXJarDirectory=\"#{prefix}/\",g", "./setlX"
     system "sed", "-i", ".original", "s,setlXlibraryPath=\"$HOME/setlXlibrary/\",setlXlibraryPath=\"#{prefix}/setlXlibrary/\",g", "./setlX"
     bin.install "setlX"
-    prefix.install %w[setlX.jar setlX-gfx.jar setlX-plot.jar setlXlibrary]
+    prefix.install %w[setlX.jar setlX-gfx.jar setlX-plot.jar setlX-stat.jar setlXlibrary]
   end
 
   test do
